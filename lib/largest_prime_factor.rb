75e53a978a9bc7dccac9319cc6ad75e7b4fb2cf1
Sean Koo <koo.sean@gmail.com>
2016-10-20 20:44:02 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+  tracker = 2
+  factors = []
+  while tracker < number
+    if number%tracker==0
+      factors.push(tracker)
+      number= number/tracker
+    else
+      tracker +=1
+    end
+  end
+  factors.push(tracker)
+#puts factors
+  return tracker
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(limit)
+    @limit = limit
+  end
+  def number
+    tracker = 2
+    factors = []
+    while tracker < @limit
+      if @limit%tracker==0
+        factors.push(tracker)
+        @limit= @limit/tracker
+      else
+        tracker +=1
+      end
+    end
+    factors.push(tracker)
+  #puts factors
+    return tracker
+  end
+end
